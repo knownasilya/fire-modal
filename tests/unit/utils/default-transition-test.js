777ff0c007f37b8b7139937c10f1c8e4cbbdfef2
Ilya Radchenko <ilya@burstcreations.com>
2015-12-23 13:34:26 -0500

    Fix util test

--- tests/unit/utils/default-transition-test.js
+++ tests/unit/utils/default-transition-test.js
@@ -4,7 +4,8 @@ import { module, test } from 'qunit';
 module('Unit | Utility | default transition');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = defaultTransition();
-  assert.ok(result);
+test('it throws if incorrect context', function(assert) {
+  assert.throws(() => {
+    defaultTransition();
+  }, new Error('[bootstrap-modal] Invalid transitions context supplied'), 'Should error if context is incorrect');
 });
