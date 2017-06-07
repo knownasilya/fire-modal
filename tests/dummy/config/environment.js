fd5c0f3fddfc57c8a704a55675f016465157bc50
Ilya Radchenko <ilya@burstcreations.com>
2017-06-07 10:46:23 -0400

    Add deploy ghpages stuff

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/bootstrap-modal';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
