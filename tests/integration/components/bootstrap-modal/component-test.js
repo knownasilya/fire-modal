eaeb7cc0688a4012dab34b783a84f6368f8f2ce2
Ilya Radchenko <ilya@burstcreations.com>
2018-12-18 18:51:28 -0500

    tests: add click close test

--- tests/integration/components/bootstrap-modal/component-test.js
+++ tests/integration/components/bootstrap-modal/component-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+import { click } from 'ember-semantic-test-helpers/test-support';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | bootstrap modal', function(hooks) {
@@ -8,15 +9,24 @@ module('Integration | Component | bootstrap modal', function(hooks) {
 
   test('it renders', async function(assert) {
     this.setProperties({
-      close() {}
+      close() {
+        assert.ok(true, 'close fired');
+      }
     });
 
     await render(hbs`
       <BootstrapModal
         @close={{this.close}}
-      />
+      as |modal|>
+        <modal.footer as |close|>
+          <button onclick={{close}} type='button'>
+            Close
+          </button>
+        </modal.footer>
+      </BootstrapModal>
     `);
 
     assert.dom('#bootstrap-modal').exists();
+    await click('Close');
   });
 });
